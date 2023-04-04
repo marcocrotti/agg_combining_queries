ba9aae0a0fffed11cccbf6141c5fd8d9d02060aa
Marco Crotti <marco.crotti@genomicsengland.co.uk>
2023-04-04 10:48:25 +0200

    refined R code

--- bin/summarise.R
+++ bin/summarise.R
@@ -15,14 +15,18 @@ par_tbl <- par_var_tbl %>%
  rename(platekey = X1, n_variants = n)
 
 var_tbl <- par_var_tbl %>%
- mutate(variant = paste(X2,X3,X4,X5, sep = "_")) %>%
+ mutate(variant = paste(X2, X3, X4, X5, sep = "_")) %>%
  group_by(variant) %>%
  count() %>%
  arrange(desc(n)) %>%
  rename(n_platekey = n)
 
-out_file_name_platekey <- paste0(sub("_results.tsv","",basename(input_table)),"_platekey_summary.tsv")
-out_file_name_variant <- paste0(sub("_results.tsv","",basename(input_table)),"_variant_summary.tsv")
+out_file_name_platekey <- paste0(sub("_results.tsv", "", basename(input_table)),
+"_platekey_summary.tsv")
+out_file_name_variant <- paste0(sub("_results.tsv", "", basename(input_table)),
+"_variant_summary.tsv")
 
-write.table(par_tbl, file = out_file_name_platekey, quote = F, sep = "\t", row.names = F)
-write.table(var_tbl, file = out_file_name_variant, quote = F, sep = "\t", row.names = F)
+write.table(par_tbl, file = out_file_name_platekey, quote = FALSE,
+sep = "\t", row.names = FALSE)
+write.table(var_tbl, file = out_file_name_variant, quote = FALSE,
+sep = "\t", row.names = FALSE)
