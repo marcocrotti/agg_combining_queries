94cfb308ff8960be663e091afd2a0f2bd1812aec
Marco Crotti <marco.crotti@genomicsengland.co.uk>
2023-08-14 16:40:40 +0200

    edit submission script typo

--- submit.sh
+++ submit.sh
@@ -13,6 +13,6 @@ module load bio/nextflow/22.10.5 tools/singularity/3.8.3
 agg_combine_query='/pgen_int_work/BRS/mcrotti/tinkering/agg_combining_queries'
 
 nextflow run "${agg_combine_query}"/main.nf \
-    --publihs_all true \
+    --publish_all true \
     -profile cluster \
     -resume
